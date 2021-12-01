2cafa3433eeeb49b7193ab425bee9998108ec4ac
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2021-12-01 14:13:09 +0400

    i did my homework

---
+++ main.js
@@ -0,0 +1,34 @@
+// display value admin
+
+let name1 = "John";
+let admin = name1;
+
+console.log(admin); // admin "John"
+
+// what will be outpud i and n;
+
+let i = 10;
+
+let n = i ++ % 5; // Output  n = 0 ; i = 10;
+
+
+// swap two variables 
+
+// let a = 10, b = 3;
+
+// let c = a;
+//  a = b;
+//  b = c 
+// alert.log('a = '+ a, 'b = ' + b) ;
+
+//  swap two variables witout third one
+
+let a = 10, b = 3;
+
+// a = a + b;
+// b = a - b ;
+// a = a - b;
+a = (a * b) / (b = a);
+
+
+console.log('a = '+ a, 'b = ' + b) ;
